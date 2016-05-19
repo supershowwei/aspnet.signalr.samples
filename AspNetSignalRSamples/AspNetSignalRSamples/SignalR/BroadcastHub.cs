31b283e3704ff9f01c60cf43229d142e1d777332
supershowwei <supershowwei@gmail.com>
2016-05-19 23:41:41 +0800

    complete signalr call with generated proxy

---
+++ AspNetSignalRSamples/AspNetSignalRSamples/SignalR/BroadcastHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AspNetSignalRSamples.SignalR
+{
+    public class BroadcastHub : Hub
+    {
+        public void Broadcast(string name, string message)
+        {
+            Clients.All.showmessage(name, message);
+        }
+    }
+}
---
+++ AspNetSignalRSamples/AspNetSignalRSamples/SignalR/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AspNetSignalRSamples.SignalR.Startup))]
+
+namespace AspNetSignalRSamples.SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
